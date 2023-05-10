a2837c65a2109d5dcf8af004b59270fd400387c3
seoseunghyeon <tmdgustj@gmail.com>
2023-05-11 01:01:11 +0900

    10818 작성

---
+++ src/Baekjoon/BaekJoon_10818.java
@@ -0,0 +1,14 @@
+package Baekjoon;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class BaekJoon_10818 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("숫자의 개수와 숫자를 입력해주세요.");
+        int size = scanner.nextInt();
+        int number = scanner.nextInt();
+    }
+}
