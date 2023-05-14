de30e421d2611d4b5618d9be3a1a0001e24402a9
ssh0820 <53414519+ssh0820@users.noreply.github.com>
2023-05-15 00:23:32 +0900

    commit

--- src/Baekjoon/BaekJoon_10818.java
+++ src/Baekjoon/BaekJoon_10818.java
@@ -1,14 +1,19 @@
 package Baekjoon;
 
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.*;
 
 public class BaekJoon_10818 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("숫자의 개수와 숫자를 입력해주세요.");
         int size = scanner.nextInt();
-        int number = scanner.nextInt();
+
+        int[] number = new int[size];
+
+        for (int i = 0; i < number.length; i++){
+            number[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(number);
+        System.out.println(number[0] + " " + number[size - 1]);
     }
 }
