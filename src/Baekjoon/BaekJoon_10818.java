04e5084e7b18093486255b47f6ffa9de77f2df91
ssh0820 <53414519+ssh0820@users.noreply.github.com>
2023-06-03 01:02:37 +0900

    commit

--- src/Baekjoon/BaekJoon_10818.java
+++ src/Baekjoon/BaekJoon_10818.java
@@ -6,14 +6,18 @@ public class BaekJoon_10818 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int size = scanner.nextInt();
+        int min = 0;
+        int max = 0;
 
         int[] number = new int[size];
 
         for (int i = 0; i < number.length; i++){
             number[i] = scanner.nextInt();
+            for (int j = 0; j < i; j++){
+                min = Math.min(number[i], min);
+                max = Math.max(number[i], max);
+            }
         }
-
-        Arrays.sort(number);
-        System.out.println(number[0] + " " + number[size - 1]);
+        System.out.println(min+" "+max);
     }
 }
